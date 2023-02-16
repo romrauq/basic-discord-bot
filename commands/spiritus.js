f5f85b5de53e62ad736254bd42f89c76e1490837
John Quarmor <johnequarmor@gmail.com>
2023-02-16 19:29:12 +0000

    Created command that secretly responds to command executor.

---
+++ commands/spiritus.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+// This command's response will only be seen by the one who executed the command:
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("spiritus")
+		.setDescription(`Responds descreetly to user who exeuted the holy command.`),
+	async execute(interaction) {
+		await interaction.reply({
+			content: "Absent his presence may feel but the Ommnissiah watches over us all",
+			ephemeral: true, // Sets the reply to be seen only by the command executor.
+		});
+	},
+};
