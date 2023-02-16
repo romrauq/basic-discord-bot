60972e1c2c35096384f104dd45ccb7898defc5de
John Quarmor <johnequarmor@gmail.com>
2023-02-16 12:29:49 +0000

    Set event listener for slash command inputs.

--- src/bot.js
+++ src/bot.js
@@ -33,4 +33,25 @@ for (const file of commandFiles) {
 	}
 }
 
+// Create listener for Client#event:interactionCreate event that will execute whenever your application receives an interaction:
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isChatInputCommand()) return;
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({
+			content: "There was an error while executing this command!",
+			ephemeral: true,
+		});
+	}
+});
+
 client.login(token);
