5708da668fd92f146784bb512088666c13d11b46
John Quarmor <johnequarmor@gmail.com>
2023-02-16 12:08:43 +0000

    created command file.

---
+++ commands/hello.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("hello")
+		.setDescription(`Replies to "/hello" slash command.`),
+	async execute(interaction) {
+		await interaction.reply("Pong!");
+	},
+};
