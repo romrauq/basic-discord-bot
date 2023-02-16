289e0e2906cc745980f06280785439a25e6ddd72
John Quarmor <johnequarmor@gmail.com>
2023-02-16 12:28:24 +0000

    Set script to dynamically load command files.

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,36 @@
 // require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
+const { Client, Events, GatewayIntentBits, Collection } = require("discord.js"); // Import/destructure class from discord.js library
 const { token } = require("../config.json");
 
-const { Client, Events, GatewayIntentBits } = require("discord.js"); // Import/destructure class from discord.js library
 const client = new Client({ intents: [GatewayIntentBits.Guilds] }); // Create a new instance of the Client class:
+
+// Collection (class) is used to store & efficiently retrieve commands for execution.
+client.commands = new Collection();
 
 // An event listener that is fired when the bot successfully logs in:
 client.once(Events.ClientReady, (c) => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
+
+// Get string of path to "../commands" directory:
+const commandsPath = path.join(__dirname, "commands");
+// Get command files within "../commands" directory:
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith(".js"));
+
+// Dynamically retrieve your command files for access from this  "./bot.js" file:
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	// Set a new item in the Collection with the key as the command name and the value as the exported module
+	if ("data" in command && "execute" in command) {
+		client.commands.set(command.data.name, command);
+	} else {
+		console.log(
+			`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+		);
+	}
+}
 
 client.login(token);
