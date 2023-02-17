78803ed095c2e02b6183f007f4170ef47fe02e51
John Quarmor <johnequarmor@gmail.com>
2023-02-17 10:17:37 +0000

    Edited description text

--- commands/praise.js
+++ commands/praise.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("praise")
-		.setDescription("Gives praise to the almighty Ommnissiah."),
+		.setDescription("Gives praise to the almighty Omnissiah."),
 	async execute(interaction) {
 		await interaction.reply(
 			`${interaction.user.username} gives praise to the almighty Omnissiah!`
