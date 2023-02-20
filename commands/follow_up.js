669895c1a28a1241b6f61e41d38506c53b7070f8
John Quarmor <johnequarmor@gmail.com>
2023-02-20 11:06:30 +0000

    Set wait period between initial message and follow up message.

--- commands/follow_up.js
+++ commands/follow_up.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
+const wait = require("node:timers/promises").setTimeout;
 
 // This command's response will only be seen by the one who executed the command:
 module.exports = {
@@ -7,6 +8,7 @@ module.exports = {
 		.setDescription(`Sends a response & follows up with another response.`),
 	async execute(interaction) {
 		await interaction.reply("This is the first message.");
+		await wait(3000);
 		await interaction.followUp("And this is the second message.");
 	},
 };
