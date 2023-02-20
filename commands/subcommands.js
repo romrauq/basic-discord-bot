9fd53e874c5ddd1e0e8a5b6fa6a293d10238244e
John Quarmor <johnequarmor@gmail.com>
2023-02-20 12:44:31 +0000

    Created command, without execution function scripts.

---
+++ commands/subcommands.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { execute } = require("./validation");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("info")
+		.setDescription("Get info about a user or a server!")
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("user")
+				.setDescription("Info about a user")
+				.addUserOption((option) => option.setName("target").setDescription("The user"))
+		)
+		.addSubcommand((subcommand) =>
+			subcommand.setName("server").setDescription("Info about the server")
+		),
+	async execute(interaction) {},
+};
