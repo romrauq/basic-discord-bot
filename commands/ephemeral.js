d7215c6f415715e21fcab2b49d48d83223bf3f9c
John Quarmor <johnequarmor@gmail.com>
2023-02-20 11:05:59 +0000

    Changed command name.

--- commands/ephemeral.js
+++ commands/ephemeral.js
@@ -3,7 +3,7 @@ const { SlashCommandBuilder } = require("discord.js");
 // This command's response will only be seen by the one who executed the command:
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName("descreet")
+		.setName("ephemeral")
 		.setDescription(`Responds descreetly to the user who exeuted the command.`),
 	async execute(interaction) {
 		await interaction.reply({
