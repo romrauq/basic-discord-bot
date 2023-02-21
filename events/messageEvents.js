72082450af8187a746cfcfdd6b87aaf32e421b92
John Quarmor <johnequarmor@gmail.com>
2023-02-21 14:16:46 +0000

    Edited comment.

--- events/messageEvents.js
+++ events/messageEvents.js
@@ -1,6 +1,6 @@
 const { Events } = require("discord.js");
 
-// An event listener message events to execute code when a message is received in the channel:
+// An event listener for incoming messages received in the bot's channel:
 module.exports = {
 	name: Events.MessageCreate,
 	execute(message) {
