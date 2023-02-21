b5345135ff8d123b27b21b74d223707dfc3092a1
John Quarmor <johnequarmor@gmail.com>
2023-02-21 15:38:51 +0000

    Created condition for messages with and without image attachments.

--- events/messageEvents.js
+++ events/messageEvents.js
@@ -5,9 +5,19 @@ module.exports = {
 	name: Events.MessageCreate,
 	execute(message) {
 		try {
-			console.log(message);
+			if (message.attachments.size === 0) {
+				// if there are no attachments in the received message:
+				const message_content = message.content; // Save the message content.
+				console.log(`Message text content: \n"${message_content}"`); // log the received message.
+				console.log("No attachments found.\n");
+			} else {
+				// if there are attachments in the received message:
+				console.log("Attachment(s) found.");
+				const attachment = message.attachments.first(); // Get the attachment object
+				console.log(`Image URL:	\n${attachment.url}\n`); // Log the URL of the image attachment.
+			}
 		} catch (error) {
-			console.error(error);
+			console.error(error); // Log any error that may have occurred.
 		}
 	},
 };
